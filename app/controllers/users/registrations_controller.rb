4bbe344a0d71800ed693a31b1468816452119fcf
Uthaeus <romanlavery@gmail.com>
2023-05-30 15:41:20 -0600

    added jti column to users, added sign in/up methods in devise controllers

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,34 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
+  respond_to :json
+
+  protected 
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+  end
+
+  private
+
+  def respond_with(resource, _opts = {})
+    if request.method === "POST" && resource.persisted?
+      render json: {
+        status: {code: 200, message: 'Signed up sucessfully.'},
+        data: resource
+      }, status: :ok
+    elsif request.method === "DELETE"
+      render json: {
+        status: {code: 200, message: 'Account deleted sucessfully.'}
+      }, status: :ok
+    else
+      render json: {
+        status: {code: 422, message: "User could not be created successfully. #{resource.errors.full_messages.to_sentence}"}
+      }, status: :unprocessable_entity
+    end
+  end
+
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
