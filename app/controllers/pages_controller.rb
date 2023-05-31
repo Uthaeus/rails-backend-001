a69483a935631cabecfa3c801a315fec1206b305
Uthaeus <romanlavery@gmail.com>
2023-05-31 12:12:26 -0600

    working on uploading avatar images for users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,6 @@ class PagesController < ApplicationController
   respond_to :json
 
   def home
-    @welcome_message = "Welcome to the home page!"
-
-    render json: { message: @welcome_message }
   end
 
   def user_check
