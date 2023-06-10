198f6395300427ffcc580b77b96ced10e85fb809
Uthaeus <romanlavery@gmail.com>
2023-06-09 23:16:30 -0600

    refactored create method in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,31 +1,30 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :update, :destroy]
+  before_action :authenticate_user!, only: [:create, :destroy]
 
   respond_to :json
 
   def index
     @comments = Comment.all
-    respond_with(@comments)
+    render json: @comments, include: :user
   end
 
   def show
-    respond_with(@comment)
+    render json: @comment, include: :user
   end
 
   def create
     @comment = Comment.new(comment_params)
     @comment.save
-    respond_with(@comment)
+    render json: @comment, include: :user
   end
 
   def update
-    @comment.update(comment_params)
-    respond_with(@comment)
   end
 
   def destroy
     @comment.destroy
-    respond_with(@comment)
+    render json: @comment, include: :user
   end
 
   private
@@ -34,6 +33,6 @@ class CommentsController < ApplicationController
     end
 
     def comment_params
-      params.require(:comment).permit(:user_id, :blog_id)
+      params.require(:comment).permit(:user_id, :blog_id, :content)
     end
 end
